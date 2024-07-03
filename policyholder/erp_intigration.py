120ec9734435d5070c3d6a5e2e82812f9d476185
ajay-kushvah-wtt <146094360+ajay-kushvah-wtt@users.noreply.github.com>
2024-07-03 12:24:22 +0530

    erp integration

--- policyholder/erp_intigration.py
+++ policyholder/erp_intigration.py
@@ -53,30 +53,49 @@ def get_value_from_mapping(obj, field_path):
             return None
     return obj
 
+def erp_mapping_data(phcp, is_vendor, account_payable_id=None):
+    mapping_dict = {
+        "name": phcp.policy_holder.trade_name,
+        "partner_type": phcp.contribution_plan_bundle.partner_type,
+        "email": phcp.policy_holder.email,
+        "phone": phcp.policy_holder.phone,
+        "mobile": phcp.policy_holder.phone,
+        "address": phcp.policy_holder.address["address"],
+        "city": None,
+        "zip": None,
+        "state_id": None,
+        "is_customer": True,
+        "is_vendor": is_vendor,
+        "country_id": 2,
+        "account_receivable_id": phcp.contribution_plan_bundle.account_receivable_id,
+        "account_payable_id": account_payable_id,
+    }
+    return mapping_dict
+
 def erp_create_update_policyholder(phcp):
     logger.debug(" ======    erp_create_update_policyholder - start    =======")
-    policyholder_data = {}
+    policyholder_data = erp_mapping_data(phcp, False)
     
-    for key, field_path in mapping_dict.items():
-        policyholder_data[key] = get_value_from_mapping(phcp, field_path)
+    # for key, field_path in mapping_dict.items():
+    #     policyholder_data[key] = get_value_from_mapping(phcp, field_path)
         
-    policyholder_data.update(static_values_policyholder)
+    # policyholder_data.update(static_values_policyholder)
     
     if phcp.policy_holder.erp_partner_access_id:
         #TODO: call update partner api
         logger.debug(" ======    erp_create_update_policyholder - update    =======")
-        logger.debug(f" ======    erp_create_update_policyholder : policyholder_data : {policyholder_data}    =======")
-        print(policyholder_data)
+        url = '{}/update/partner/{}'.format(erp_url, phcp.policy_holder.erp_partner_access_id)
+        logger.debug(f" ======    erp_create_update_policyholder : url : {url}    =======")
     else:
         logger.debug(" ======    erp_create_update_policyholder - create    =======")
         #TODO: call update partner api
         url = '{}/create/partner'.format(erp_url)
         logger.debug(f" ======    erp_create_update_policyholder : url : {url}    =======")
-        logger.debug(f" ======    erp_create_update_policyholder : policyholder_data : {policyholder_data}    =======")
-        print(policyholder_data)
-        response = requests.post(url, headers=headers, json=policyholder_data, verify=False)
-        logger.debug(f" ======    erp_create_update_policyholder : response.status_code : {response.status_code}    =======")
-        logger.debug(f" ======    erp_create_update_policyholder : response.json : {response.json()}    =======")
+    logger.debug(f" ======    erp_create_update_policyholder : policyholder_data : {policyholder_data}    =======")
+    print(policyholder_data)
+    response = requests.post(url, headers=headers, json=policyholder_data, verify=False)
+    logger.debug(f" ======    erp_create_update_policyholder : response.status_code : {response.status_code}    =======")
+    logger.debug(f" ======    erp_create_update_policyholder : response.json : {response.json()}    =======")
     logger.debug(" ======    erp_create_update_policyholder - end    =======")
     return True
 
