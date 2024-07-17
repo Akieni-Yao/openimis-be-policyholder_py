015a77f083f51c1d7dc3eca0cf370e86e683671b
LAKSHYA SONI <lakshya.soni@walkingtree.tech>
2024-07-17 15:06:01 +0530

    uncommented code for erp

--- policyholder/urls.py
+++ policyholder/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from policyholder import views
+from policyholder import views, erp_intigration
 
 urlpatterns = [
     path("imports/<policy_holder_code>/policyholderinsurees", views.import_phi),
@@ -11,6 +11,6 @@ urlpatterns = [
     path('portal-reset/<uidb64>/<token>/<e_timestamp>/', views.portal_reset),
     path('deactivate-not-submitted-request/', views.deactivate_not_submitted_request),
     
-    # path('create-existing-policyholder-in-erp/', erp_intigration.create_existing_policyholder_in_erp),
-    # path('create-existing-fosa-in-erp/', erp_intigration.create_existing_fosa_in_erp),
+    path('create-existing-policyholder-in-erp/', erp_intigration.create_existing_policyholder_in_erp),
+    path('create-existing-fosa-in-erp/', erp_intigration.create_existing_fosa_in_erp),
 ]
