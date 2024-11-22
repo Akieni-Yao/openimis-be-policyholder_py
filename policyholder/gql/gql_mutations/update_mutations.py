f1b66342475fa52eab0ba0f89dc4d06f854c017d
tom hikari <tomprescott1565@gmail.com>
2024-11-22 16:58:08 +0100

    change email template

--- policyholder/gql/gql_mutations/update_mutations.py
+++ policyholder/gql/gql_mutations/update_mutations.py
@@ -4,15 +4,27 @@ import graphene
 from django.db.models import Q
 
 from core.constants import POLICYHOLDER_UPDATE_NT
-from core.gql.gql_mutations.base_mutation import BaseMutation, BaseHistoryModelUpdateMutationMixin
+from core.gql.gql_mutations.base_mutation import (
+    BaseMutation,
+    BaseHistoryModelUpdateMutationMixin,
+)
 from core.models import InteractiveUser
 from core.notification_service import create_camu_notification
 from payment.models import Payment, PaymentPenaltyAndSanction
 from policyholder.apps import PolicyholderConfig
-from policyholder.models import PolicyHolder, PolicyHolderInsuree, PolicyHolderContributionPlan, PolicyHolderUser
-from policyholder.gql.gql_mutations import PolicyHolderInsureeUpdateInputType, \
-    PolicyHolderContributionPlanUpdateInputType, PolicyHolderUserUpdateInputType, PolicyHolderUpdateInputType, \
-    PHApprovalInput
+from policyholder.models import (
+    PolicyHolder,
+    PolicyHolderInsuree,
+    PolicyHolderContributionPlan,
+    PolicyHolderUser,
+)
+from policyholder.gql.gql_mutations import (
+    PolicyHolderInsureeUpdateInputType,
+    PolicyHolderContributionPlanUpdateInputType,
+    PolicyHolderUserUpdateInputType,
+    PolicyHolderUpdateInputType,
+    PHApprovalInput,
+)
 from policyholder.validation import PolicyHolderValidation
 from policyholder.validation.permission_validation import PermissionValidation
 from policyholder.constants import *
@@ -23,6 +35,7 @@ from django.core.mail import EmailMessage
 from django.conf import settings
 from django.utils import timezone
 from policyholder.erp_intigration import erp_create_update_policyholder
+from policyholder.portal_utils import send_approved_or_rejected_email
 
 logger = logging.getLogger(__name__)
 
@@ -38,21 +51,28 @@ class UpdatePolicyHolderMutation(BaseHistoryModelUpdateMutationMixin, BaseMutati
     @classmethod
     def _validate_mutation(cls, user, **data):
         super()._validate_mutation(user, **data)
-        PermissionValidation.validate_perms(user, PolicyholderConfig.gql_mutation_update_policyholder_perms)
+        PermissionValidation.validate_perms(
+            user, PolicyholderConfig.gql_mutation_update_policyholder_perms
+        )
         PolicyHolderValidation.validate_update(user, **data)
-        
+
         contributionPlan = PolicyHolderContributionPlan.objects.filter(
-        policy_holder__id=data['id'], is_deleted=False).first()        
-        
-        if contributionPlan is not None and contributionPlan.contribution_plan_bundle is not None:
-            cpId= contributionPlan.contribution_plan_bundle.id
+            policy_holder__id=data["id"], is_deleted=False
+        ).first()
+
+        if (
+            contributionPlan is not None
+            and contributionPlan.contribution_plan_bundle is not None
+        ):
+            cpId = contributionPlan.contribution_plan_bundle.id
             print(f"********************** UPDATE POLICIY HOLDER ERP BEGIN {cpId}")
-            erp_create_update_policyholder(data['id'], cpId, user)
+            erp_create_update_policyholder(data["id"], cpId, user)
             print("*********************** UPDATE POLICIY HOLDER ERP END")
 
 
-
-class UpdatePolicyHolderInsureeMutation(BaseHistoryModelUpdateMutationMixin, BaseMutation):
+class UpdatePolicyHolderInsureeMutation(
+    BaseHistoryModelUpdateMutationMixin, BaseMutation
+):
     _mutation_class = "PolicyHolderInsureeMutation"
     _mutation_module = "policyholder"
     _model = PolicyHolderInsuree
@@ -63,10 +83,14 @@ class UpdatePolicyHolderInsureeMutation(BaseHistoryModelUpdateMutationMixin, Bas
     @classmethod
     def _validate_mutation(cls, user, **data):
         super()._validate_mutation(user, **data)
-        PermissionValidation.validate_perms(user, PolicyholderConfig.gql_mutation_update_policyholderinsuree_perms)
+        PermissionValidation.validate_perms(
+            user, PolicyholderConfig.gql_mutation_update_policyholderinsuree_perms
+        )
 
 
-class UpdatePolicyHolderContributionPlanMutation(BaseHistoryModelUpdateMutationMixin, BaseMutation):
+class UpdatePolicyHolderContributionPlanMutation(
+    BaseHistoryModelUpdateMutationMixin, BaseMutation
+):
     _mutation_class = "PolicyHolderContributionPlanMutation"
     _mutation_module = "policyholder"
     _model = PolicyHolderContributionPlan
@@ -77,8 +101,10 @@ class UpdatePolicyHolderContributionPlanMutation(BaseHistoryModelUpdateMutationM
     @classmethod
     def _validate_mutation(cls, user, **data):
         super()._validate_mutation(user, **data)
-        PermissionValidation.validate_perms(user,
-                                            PolicyholderConfig.gql_mutation_update_policyholdercontributionplan_perms)
+        PermissionValidation.validate_perms(
+            user,
+            PolicyholderConfig.gql_mutation_update_policyholdercontributionplan_perms,
+        )
 
 
 class UpdatePolicyHolderUserMutation(BaseHistoryModelUpdateMutationMixin, BaseMutation):
@@ -89,10 +115,10 @@ class UpdatePolicyHolderUserMutation(BaseHistoryModelUpdateMutationMixin, BaseMu
     @classmethod
     def _mutate(cls, user, **data):
         if "client_mutation_id" in data:
-            data.pop('client_mutation_id')
+            data.pop("client_mutation_id")
         if "client_mutation_label" in data:
-            data.pop('client_mutation_label')
-        updated_object = cls._model.objects.filter(id=data['id']).first()
+            data.pop("client_mutation_label")
+        updated_object = cls._model.objects.filter(id=data["id"]).first()
         [setattr(updated_object, key, data[key]) for key in data]
         cls.update_policy_holder_user(user=user, object_to_update=updated_object)
 
@@ -107,7 +133,9 @@ class UpdatePolicyHolderUserMutation(BaseHistoryModelUpdateMutationMixin, BaseMu
     @classmethod
     def _validate_mutation(cls, user, **data):
         super()._validate_mutation(user, **data)
-        PermissionValidation.validate_perms(user, PolicyholderConfig.gql_mutation_update_policyholderuser_perms)
+        PermissionValidation.validate_perms(
+            user, PolicyholderConfig.gql_mutation_update_policyholderuser_perms
+        )
 
 
 class UpdatePolicyHolderInsureeDesignation(graphene.Mutation):
@@ -127,11 +155,11 @@ class UpdatePolicyHolderInsureeDesignation(graphene.Mutation):
             record = PolicyHolderInsuree.objects.get(
                 policy_holder__id=policy_holder.id,
                 insuree__id=insuree_id,
-                is_deleted=False
+                is_deleted=False,
             )
             json_ext_data = record.json_ext
             if flag:
-                json_ext_data['designation'] = designation
+                json_ext_data["designation"] = designation
                 record.json_ext = json_ext_data
             else:
                 record.json_ext = json_ext_data
@@ -165,8 +193,10 @@ class PHApprovalMutation(graphene.Mutation):
             is_rejected = input.is_rejected
             is_rework = input.is_rework
 
-            ph_obj = PolicyHolder.objects.filter(id=ph_id, request_number=request_number).first()
-            
+            ph_obj = PolicyHolder.objects.filter(
+                id=ph_id, request_number=request_number
+            ).first()
+
             print(f"********************** UPDATE POLICIY HOLDER ERP BEGIN {ph_obj}")
 
             if ph_obj:
@@ -178,14 +208,24 @@ class PHApprovalMutation(graphene.Mutation):
                     ph_obj.is_approved = True
                     ph_obj.status = PH_STATUS_APPROVED
                     ph_obj.save(username=username)
-                    rename_folder_dms_and_openkm(ph_obj.request_number, generated_number)
+                    rename_folder_dms_and_openkm(
+                        ph_obj.request_number, generated_number
+                    )
                     message = "Policy Holder Request Successfully Approved."
-                    print(f"********************** UPDATE POLICIY HOLDER ERP END APPROVED {ph_obj}")
+                    email_body = (
+                        "Your Policy Holder Request has been Successfully Approved."
+                    )
+                    subject = (
+                        "CAMU, Your Policyholder Application request has been approved."
+                    )
+                    print(
+                        f"********************** UPDATE POLICIY HOLDER ERP END APPROVED {ph_obj}"
+                    )
                 elif is_rejected:
                     ph_obj.is_rejected = True
                     ph_obj.status = PH_STATUS_REJECTED
                     ph_obj.rejected_reason = input.rejected_reason
-                    # ph_obj.is_deleted = True 
+                    # ph_obj.is_deleted = True
                     ph_obj.save(username=username)
 
                     # phu_obj = PolicyHolderUser.objects.filter(policy_holder=ph_obj).first()
@@ -194,13 +234,18 @@ class PHApprovalMutation(graphene.Mutation):
                     # InteractiveUser.objects.filter(id=phu_obj.user.i_user.id).update(validity_to=timezone.now())
 
                     message = "Policy Holder Request Rejected."
-                    subject = "CAMU, Your Policyholder Application request has been rejected."
-                    email_body = policyholder_reject.format(
-                        request_number=ph_obj.request_number,
-                        contact_name=ph_obj.contact_name.get("contactName"),
-                        rejection_reason=ph_obj.rejected_reason
+                    subject = (
+                        "CAMU, Your Policyholder Application request has been rejected."
                     )
-                    print(f"********************** UPDATE POLICIY HOLDER ERP END REJECTED {ph_obj}")
+                    email_body = f"Your Policy Holder Request has been Rejected. Reason: {ph_obj.rejected_reason}"
+                    # email_body = policyholder_reject.format(
+                    #     request_number=ph_obj.request_number,
+                    #     contact_name=ph_obj.contact_name.get("contactName"),
+                    #     rejection_reason=ph_obj.rejected_reason,
+                    # )
+                    print(
+                        f"********************** UPDATE POLICIY HOLDER ERP END REJECTED {ph_obj}"
+                    )
                 elif is_rework:
                     ph_obj.is_rework = True
                     ph_obj.is_submit = False
@@ -210,24 +255,41 @@ class PHApprovalMutation(graphene.Mutation):
                     ph_obj.save(username=username)
                     message = "Policy Holder Request Sent for Rework."
                     subject = "CAMU, Your Policyholder Application need some rework."
-                    email_body = policyholder_rework.format(
-                        request_number=ph_obj.request_number,
-                        contact_name=ph_obj.contact_name.get("contactName"),
-                        rework_comment=ph_obj.rework_comment
+                    email_body = (
+                        f"Your Policy Holder Request has been Sent for Rework. Reason: {ph_obj.rework_comment}"
                     )
-                    print(f"********************** UPDATE POLICIY HOLDER ERP END REWORK {ph_obj}")
+                    # email_body = policyholder_rework.format(
+                    #     request_number=ph_obj.request_number,
+                    #     contact_name=ph_obj.contact_name.get("contactName"),
+                    #     rework_comment=ph_obj.rework_comment,
+                    # )
+                    print(
+                        f"********************** UPDATE POLICIY HOLDER ERP END REWORK {ph_obj}"
+                    )
                 else:
                     success = False
                     message = "Policy Holder request action is not valid."
 
                 if ph_obj.email and subject and email_body:
-                    email_message = EmailMessage(subject, email_body, settings.EMAIL_HOST_USER, [ph_obj.email])
-                    email_message.send()
-                    
+                    send_approved_or_rejected_email(
+                        {
+                            "last_name": ph_obj.contact_name.get("contactName"),
+                            "email": ph_obj.email,
+                        },
+                        subject,
+                        email_body,
+                    )
+                    # email_message = EmailMessage(subject, email_body, settings.EMAIL_HOST_USER, [ph_obj.email])
+                    # email_message.send()
+
                 try:
-                    print(f"********************** UPDATE POLICIY HOLDER ERP NOTIFICATION {ph_obj}")
+                    print(
+                        f"********************** UPDATE POLICIY HOLDER ERP NOTIFICATION {ph_obj}"
+                    )
                     create_camu_notification(POLICYHOLDER_UPDATE_NT, ph_obj)
-                    logger.info("Successfully created CAMU notification with POLICYHOLDER_CREATION_NT.")
+                    logger.info(
+                        "Successfully created CAMU notification with POLICYHOLDER_CREATION_NT."
+                    )
                 except Exception as e:
                     logger.error(f"Failed to create CAMU notification: {e}")
             else:
@@ -253,34 +315,45 @@ class UnlockPolicyHolderMutation(graphene.Mutation):
         try:
             policyholder = PolicyHolder.objects.get(id=policy_holder)
         except PolicyHolder.DoesNotExist:
-            return UnlockPolicyHolderMutation(success=False, message="Policyholder does not exist.")
+            return UnlockPolicyHolderMutation(
+                success=False, message="Policyholder does not exist."
+            )
 
         # Query all payments associated with the policyholder's contracts
-        payments = Payment.objects.filter(
-            Q(contract__policy_holder__id=policy_holder)
-        )
+        payments = Payment.objects.filter(Q(contract__policy_holder__id=policy_holder))
 
         # Check if any payments exist for the policyholder
         if not payments.exists():
-            return UnlockPolicyHolderMutation(success=False, message="No payments found for this policyholder.")
+            return UnlockPolicyHolderMutation(
+                success=False, message="No payments found for this policyholder."
+            )
 
-        if payments.filter(status__ne=5).exists():  # Check if any payment is not fully paid
-            return UnlockPolicyHolderMutation(success=False, message="Not all payments are fully paid.")
+        if payments.filter(
+            status__ne=5
+        ).exists():  # Check if any payment is not fully paid
+            return UnlockPolicyHolderMutation(
+                success=False, message="Not all payments are fully paid."
+            )
 
         # Check penalties of those payments: should have status 3 or 4, penalty_type 'Penalty', and is_approved=True
         penalties = PaymentPenaltyAndSanction.objects.filter(
-            Q(payment__in=payments) &
-            ~Q(status__in=[3, 4])
+            Q(payment__in=payments) & ~Q(status__in=[3, 4])
         )
 
         # If penalties exist that are unresolved or not approved
         if penalties.exists():
             if not penalties.filter(is_approved=True).exists():
-                return UnlockPolicyHolderMutation(success=False, message="Penalties are not approved.")
-            return UnlockPolicyHolderMutation(success=False, message="Penalties are not fully resolved.")
+                return UnlockPolicyHolderMutation(
+                    success=False, message="Penalties are not approved."
+                )
+            return UnlockPolicyHolderMutation(
+                success=False, message="Penalties are not fully resolved."
+            )
 
         # If all payments are fully paid, penalties are resolved and approved, unlock the policyholder
-        policyholder.status = 'Approved'
+        policyholder.status = "Approved"
         policyholder.save()
 
-        return UnlockPolicyHolderMutation(success=True, message="Policyholder unlocked successfully.")
+        return UnlockPolicyHolderMutation(
+            success=True, message="Policyholder unlocked successfully."
+        )
